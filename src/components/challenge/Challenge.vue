cf82a99c21bc746be8ac73a229d828d3265540db
Renan Gomes <renan.gomes@live.com>
2019-09-26 11:11:51 -0300

    Removing initial stylization of the component.

--- src/components/challenge/Challenge.vue
+++ src/components/challenge/Challenge.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="challenge-info">
-    <div class="version">{{ challenge.version }}</div>
     <div class="details">
       <h2>{{ challenge.title }}</h2>
       <p class="description">{{ challenge.small_description }}</p>
@@ -30,45 +29,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.challenge-info,
-.challenge-info .version,
-.challenge-info .details {
-  display: flex
-}
-
-.challenge-info {
-  border: 1px solid red;
-  margin: 1vh 1vw;
-  border-radius: 10px;
-  flex-flow: column
-}
-.challenge-info .version {
-  background-color: #eb007d;
-  color: #fff;
-  font-weight: bold;
-  font-size: 2em;
-  align-items: center;
-  justify-content: center;
-  width: 50px;
-  border-top-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  user-select: none
-}
-.challenge-info .details {
-  flex-flow: column wrap;
-  padding: 1vh 2.5%;
-  width: 100%
-}
-
-.challenge-info .details h2,
-.challenge-info .details .deadline {
-  color: #eb007d
-}
-
-.challenge-info .details .description {
-  margin: 10px 0
-}
-</style>
-
