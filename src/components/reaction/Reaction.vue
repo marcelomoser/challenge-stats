0c74bd52f14af69bf49c881b5a005d102345e02f
Renan Gomes <renan.gomes@live.com>
2019-09-30 07:14:23 -0300

    Added styles for reaction, reaction list and pull request.

--- src/components/reaction/Reaction.vue
+++ src/components/reaction/Reaction.vue
@@ -1,14 +1,12 @@
 <style src="./reaction.styl" lang="stylus" scoped></style>
 
 <template>
-  <div>
-    <div>
-      <img :src="user.avatarUrl" :alt="user.login" height="40" width="40">
-    </div>
-    <div>
+  <div class="reaction">
+    <img class="reaction__user-avatar" :src="user.avatarUrl" :alt="user.login">
+    <div class="reaction__info">
       <p>
         <a :href="user.url">@{{ user.login }}</a>
-        reagiu a esta pull com {{ reaction.content | toEmoji }}
+        reagiu a este pull request com {{ reaction.content | toEmoji }}
         {{ reaction.createdAt | toTextFromNow }}
       </p>
     </div>
--- src/components/reaction/ReactionList.vue
+++ src/components/reaction/ReactionList.vue
@@ -1,17 +1,27 @@
 <style src="./reaction-list.styl" lang="stylus" scoped></style>
 
 <template>
-  <div class="reactions" v-if="reactions && reactions.totalCount > 0">
-    <p class="total-reactions">Este pull request recebeu {{ votes }}</p>
-    <p class="unique-reactions">Votantes únicos: {{ uniqueReactions.length }}</p>
-    <div>
+  <div class="reaction-list" v-if="reactions && reactions.totalCount > 0">
+    <div class="reaction-list__counter">
+      <p class="reaction-list__counter__total"
+         title="Nº total de reações no pull request (contabilizando reações duplicadas).">
+        Reações<br>
+        <span>{{ reactions.totalCount }}</span>
+      </p>
+      <p class="reaction-list__counter__unique"
+        title="Este é o número de reações únicas por usuário.">
+        Reações Únicas<br>
+        <span>{{ uniqueReactions.length }}</span>
+      </p>
+    </div>
+    <div class="reaction-list__users">
       <Reaction
         v-for="reaction in reactions.nodes"
         :key="reaction.id"
         :reaction="reaction" />
     </div>
   </div>
-  <div class="reactions" v-else>Ninguém votou nesse pull request ainda</div>
+  <div class="reaction-list" v-else>Ninguém votou nesse pull request ainda</div>
 </template>
 
 <script>
@@ -26,10 +36,6 @@ export default {
   computed: {
     uniqueReactions(){
       return uniqueReactionsByUserLogin(this.reactions)
-    },
-    votes(){
-      const reactions = this.reactions.totalCount
-      return `${reactions} vot${reactions === 1 ? 'o': 'os'}`
     }
   }
 }
